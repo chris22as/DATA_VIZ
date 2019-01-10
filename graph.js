4843a528c976c8f0ea36056562478de7939b21ca
chris22as <32392486+chris22as@users.noreply.github.com>
2019-01-10 10:41:57 +0100

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,579 @@
+
+
+var relations=  [
+  {
+    "source": "Cedric Dumas",
+    "target": "Emilio Loren Roth Monzon",
+    "weigth": 20
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Amine Chellali",
+    "weigth": 14
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Isabelle Milleville-Pennel",
+    "weigth": 10
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Eric Nouri",
+    "weigth": 2
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Stephane Huot",
+    "weigth": 4
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Pierre Dragicevic",
+    "weigth": 2
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Gerard Hegron",
+    "weigth": 3
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Jean-Daniel Fekete",
+    "weigth": 1
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Frederic Jourdan",
+    "weigth": 1
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Boris Takac",
+    "weigth": 1
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Cristol Grosdemouge et al.",
+    "weigth": 1
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Caroline G. L. Cao",
+    "weigth": 2
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Lepreux Sophie",
+    "weigth": 1
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Patrick Girard",
+    "weigth": 1
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Guillaume Gamelin",
+    "weigth": 1
+  },
+  {
+    "source": "Cedric Dumas",
+    "target": "Samir Otmane",
+    "weigth": 1
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Isabelle Milleville-Pennel",
+    "weigth": 10
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Eric Nouri",
+    "weigth": 2
+  },
+  {
+    "source": "Isabelle Milleville-Pennel",
+    "target": "Boris Takac",
+    "weigth": 1
+  },
+  {
+    "source": "Isabelle Milleville-Pennel",
+    "target": "Cristol Grosdemouge et al.",
+    "weigth": 1
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Boris Takac",
+    "weigth": 1
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Cristol Grosdemouge et al.",
+    "weigth": 1
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Caroline G. L. Cao",
+    "weigth": 2
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Guillaume Gamelin",
+    "weigth": 1
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Samir Otmane",
+    "weigth": 1
+  },
+  {
+    "source": "Stephane Huot",
+    "target": "Pierre Dragicevic",
+    "weigth": 2
+  },
+  {
+    "source": "Gerard Hegron",
+    "target": "Pierre Dragicevic",
+    "weigth": 1
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Frederic Jourdan",
+    "weigth": 1
+  },
+  {
+    "source": "Amine Chellali",
+    "target": "Emilio Loren Roth Monzon",
+    "weigth": 2
+  },
+  {
+    "source": "Emilio Loren Roth Monzon",
+    "target": "Caroline G. L. Cao",
+    "weigth": 2
+  },
+  {
+    "source": "Stephane Huot",
+    "target": "Gerard Hegron",
+    "weigth": 3
+  },
+  {
+    "source": "Isabelle Milleville-Pennel",
+    "target": "Eric Nouri",
+    "weigth": 2
+  },
+  {
+    "source": "Lepreux Sophie",
+    "target": "Patrick Girard",
+    "weigth": 1
+  },
+  {
+    "source": "Stephane Huot",
+    "target": "Jean-Daniel Fekete",
+    "weigth": 1
+  },
+  {
+    "source": "Gerard Hegron",
+    "target": "Jean-Daniel Fekete",
+    "weigth": 1
+  },
+  {
+    "source": "Gerard Hegron",
+    "target": "Pierre Dragicevic",
+    "weigth": 1
+  }
+  ]
+
+
+var names= [
+  
+  {
+   
+   "auteurs": "Cedric Dumas",
+    "id": 1,
+    "in": 16,
+    "out": 16,
+    "domaine": "Automatique Productique Informatique",
+    "Organisme": "IMT Atlantique",
+    "Ville": "Nantes",
+    "domaine_label": 1,
+    "organisme_label": 3,
+    "ville_label": 5
+  },
+  {
+   
+    "auteurs": "Amine Chellali",
+    "id": 2,
+    "in": 10,
+    "out": 10,
+    "domaine": "Informatique Bio-informatique Syst%uFFFDmes Complexes",
+    "Organisme": "IBISC",
+    "Ville": "Evry",
+    "domaine_label": 7,
+    "organisme_label": 2,
+    "ville_label": 3
+  },
+  {
+   
+    "auteurs": "Isabelle Milleville-Pennel",
+    "id": 3,
+    "in": 5,
+    "out": 5,
+    "domaine": "Communication Cybern%uFFFDtique",
+    "Organisme": "IRCCYN",
+    "Ville": "Nantes",
+    "domaine_label": 2,
+    "organisme_label": 5,
+    "ville_label": 5
+  },
+  {
+   
+    "auteurs": "Stephane Huot",
+    "id": 4,
+    "in": 4,
+    "out": 4,
+    "domaine": "Human Computer Interaction",
+    "Organisme": "INRIA",
+    "Ville": "Lille",
+    "domaine_label": 5,
+    "organisme_label": 4,
+    "ville_label": 4
+  },
+  {
+    
+    "auteurs": "Gerard Hegron",
+    "id": 5,
+    "in": 5,
+    "out": 5,
+    "domaine": "Architecture",
+    "Organisme": "CERMA UMR CNRS 1563",
+    "Ville": "Nantes",
+    "domaine_label": 0,
+    "organisme_label": 0,
+    "ville_label": 5
+  },
+  {
+    
+    "auteurs": "Emilio Loren Roth Monzon",
+    "id": 6,
+    "in": 3,
+    "out": 3,
+    "domaine": "Communication Cybern%uFFFDtique",
+    "Organisme": "IRCCYN",
+    "Ville": "Nantes",
+    "domaine_label": 2,
+    "organisme_label": 5,
+    "ville_label": 5
+  },
+  {
+   
+    "auteurs": "Lepreux Sophie",
+    "id": 7,
+    "in": 2,
+    "out": 2,
+    "domaine": "Human Computer Interaction",
+    "Organisme": "LAMIH",
+    "Ville": "Valenciennes",
+    "domaine_label": 5,
+    "organisme_label": 6,
+    "ville_label": 9
+  },
+  {
+   
+    "auteurs": "Eric Nouri",
+    "id": 8,
+    "in": 3,
+    "out": 3,
+    "domaine": "Communication Cybern%uFFFDtique",
+    "Organisme": "IRCCYN",
+    "Ville": "Nantes",
+    "domaine_label": 2,
+    "organisme_label": 5,
+    "ville_label": 5
+  },
+  {
+    
+    "auteurs": "Pierre Dragicevic",
+    "id": 9,
+    "in": 4,
+    "out": 4,
+    "domaine": "Human Computer Interaction",
+    "Organisme": "Telecom Paristech",
+    "Ville": "Paris",
+    "domaine_label": 5,
+    "organisme_label": 8,
+    "ville_label": 6
+  },
+  {
+   
+    "auteurs": "Jean-Daniel Fekete",
+    "id": 10,
+    "in": 3,
+    "out": 3,
+    "domaine": "Data Visualization",
+    "Organisme": "INRIA",
+    "Ville": "Saclay",
+    "domaine_label": 3,
+    "organisme_label": 4,
+    "ville_label": 8
+  },
+  {
+   
+    "auteurs": "Frederic Jourdan",
+    "id": 11,
+    "in": 2,
+    "out": 2,
+    "domaine": "Automatique Productique Informatique",
+    "Organisme": "IMT Atlantique",
+    "Ville": "Nantes",
+    "domaine_label": 1,
+    "organisme_label": 3,
+    "ville_label": 5
+  },
+  {
+    
+    "auteurs": "Boris Takac",
+    "id": 12,
+    "in": 3,
+    "out": 3,
+    "domaine": "Healthcare Robotics",
+    "Organisme": "Technische Universiteit",
+    "Ville": "Eindhoven",
+    "domaine_label": 4,
+    "organisme_label": 7,
+    "ville_label": 2
+  },
+  {
+   
+    "auteurs": "Cristol Grosdemouge et al.",
+    "id": 13,
+    "in": 3,
+    "out": 3,
+    "domaine": "Human Factors Engineering in Medical Systems",
+    "Organisme": "Tufts University",
+    "Ville": "Boston",
+    "domaine_label": 6,
+    "organisme_label": 9,
+    "ville_label": 0
+  },
+  {
+    
+    "auteurs": "Caroline G. L. Cao",
+    "id": 14,
+    "in": 3,
+    "out": 3,
+    "domaine": "Human Factors Engineering in Medical Systems",
+    "Organisme": "Wright State University",
+    "Ville": "Dayton",
+    "domaine_label": 6,
+    "organisme_label": 10,
+    "ville_label": 1
+  },
+  {
+    
+    "auteurs": "Patrick Girard",
+    "id": 15,
+    "in": 2,
+    "out": 2,
+    "domaine": "Human Computer Interaction",
+    "Organisme": "ENSMA",
+    "Ville": "Poitiers",
+    "domaine_label": 5,
+    "organisme_label": 1,
+    "ville_label": 7
+  },
+  {
+   
+    "auteurs": "Guillaume Gamelin",
+    "id": 16,
+    "in": 2,
+    "out": 2,
+    "domaine": "Informatique Bio-informatique Syst%uFFFDmes Complexes",
+    "Organisme": "IBISC",
+    "Ville": "Evry",
+    "domaine_label": 7,
+    "organisme_label": 2,
+    "ville_label": 3
+  },
+  {
+    
+    "auteurs": "Samir Otmane",
+    "id": 17,
+    "in": 2,
+    "out": 2,
+    "domaine": "Informatique Bio-informatique Syst%uFFFDmes Complexes",
+    "Organisme": "IBISC",
+    "Ville": "Evry",
+    "domaine_label": 7,
+    "organisme_label": 2,
+    "ville_label": 3
+  }
+]
+
+
+//var a=require('/data/auteurs.json');
+//var relations=require('/data/relations.json');
+//var names=require('/data/names.json');
+
+
+
+
+var w = 1280,
+    h = 600,
+    fbBlue = d3.rgb("#3b7998"),
+    fill = [fbBlue.brighter(2),fbBlue.brighter(),fbBlue,fbBlue.darker()];
+var vis = d3.select("body").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+
+// CREATION DES VARIABLES NODES - LINKS - THEMES
+
+var nodes = names
+var links = [];
+var themes=[]
+
+
+function index(names, name){
+  for(var i =0; i< names.length; i++){
+    if(names[i].auteurs===name){
+      return i ;}
+      }
+}
+
+
+
+for(var i = 0; i < relations.length; i++) {  
+            links.push({
+              source : index(names,relations[i].source),
+              target : index(names, relations[i].target),
+              weight : relations[i].weigth
+            })  
+      };
+
+
+//-------
+   
+
+
+
+/* Force paramettring */
+var force = d3.layout.force()
+    .linkDistance(100)
+    .linkStrength(0.05)
+    .gravity(1)
+    .charge(-3000)
+    .size([w, h])
+    .nodes(nodes)
+    .links(links)
+    //.linkStrength(function(x) { return x.weight })
+    .start();
+
+/*Link creation template */
+var link = vis.selectAll(".link")
+    .data(links)
+    .enter()
+    .append("line")
+    .attr("class", "link")
+    .style("stroke-width", 
+      function(d) { return (d.weight); })
+    ;
+
+/*Node creation template */
+
+
+var color = d3.scale.category10()
+
+var node = vis.selectAll("circle.node")
+    .data(nodes)
+  .enter().append("svg:circle")
+    .attr("class", "node")
+    .attr("cx", function(d) { return d.x; }) //x
+    .attr("cy", function(d) { return d.y; }) //y
+    .attr("r", 8)
+    .style("fill", function(d, i) {
+      return  color(d.domaine_label)//themes[d.id].domaine) //fill[parseInt((d.in+1)/3)];
+    })
+    .call(force.drag);
+
+
+node.append("title")
+    .text(function(d) { return "User "+d.id; });
+
+/* Start transition */
+vis.style("opacity", 1e-6)
+   .transition()
+   .duration(1000)
+   .style("opacity", 1);
+
+//Forces in action
+force.on("tick", function(e) {
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+  
+  node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+});
+
+
+
+/* Click-plosion and tooltip*/
+d3.select("body").on("dblclick", function() {
+  nodes.forEach(function(o, i) {
+    o.x += (Math.random() - .5) * 200;
+    o.y += (Math.random() - .5) * 200;
+  });
+  force.resume();
+});
+
+d3.selectAll('.node').on('click', function(d, i){
+  var d3this = d3.select(this);
+  /*if(d3this.style("fill") == '#ffa500')
+    d3this.style('fill', 'green');
+  else if(d3this.style("fill") == '#008000')
+    d3this.style("fill", fill[parseInt((d.in+1)/3)]);
+  else
+    d3this.style("fill",'orange');*/
+  d3.event.stopPropagation();
+});
+
+
+d3.selectAll(".node").on("dblclick", function(d, i){
+  d.fixed = !d.fixed;
+  d3.event.stopPropagation();
+});
+
+
+var div = d3.select("div.tooltip");
+
+d3.selectAll(".node").on("mouseover", function(d, i){
+  div.style("visibility", "visible")
+     .transition()
+     .duration(200)
+     .style("opacity", .9);
+  var html;
+  if(d.in == d.out)
+    html = d.auteurs +"<br/>"+d.domaine // +" Connections"
+  else
+    html = d.auteurs +"<br/>"+d.domaine //+"<br/>"+d.in+" Connections entrantes, "+d.out+" Connection sortante"
+  div.html(html)
+     .style("left", (d.x + 15) + "px")
+     .style("top", (d.y - 30) + "px");
+}).on("mouseout", function(d, i){
+  div.transition()
+     .duration(500)
+     .style("opacity", 0)
+     .each("end", function(){
+       div.style("visibility", "hidden")
+     });
+});
+
+
+/*var e = document.getElementById("choix");
+var strUser = e.options[e.selectedIndex].value
+console.log(strUser)*/
+
+
+
+
+
